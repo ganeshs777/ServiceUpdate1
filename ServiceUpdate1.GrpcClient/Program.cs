ec98cf40ee5594cd91a912f7c9fbcc7db93f9ec2
ganeshs777 <ganesh.p.shewale@gmail.com>
2024-04-10 23:10:12 +0530

    Added code to send file using grpc

--- ServiceUpdate1.GrpcClient/Program.cs
+++ ServiceUpdate1.GrpcClient/Program.cs
@@ -1,21 +1,36 @@
 ﻿using System.Threading.Tasks;
+using Google.Protobuf;
 using Grpc.Net.Client;
 using ServiceUpdate1.GrpcClient;
+using static ServiceUpdate1.GrpcClient.DeployUpdatesService;
 using static ServiceUpdate1.GrpcClient.UpdateService;
 
 
-// The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("http://localhost:5048");
-var client = new UpdateServiceClient(channel);
-var reply = await client.GetLatestVersionAsync(new Empty());
-Console.WriteLine("Greeting: " + reply.Version);
-var reply1 = await client.SubscribeAsync(
-    new SubscribeRequest
-    {
-        ClientName = "Client1",
-        ClientUrl = "Url1",
-        ServiceName = "Service1"
-    }) ;
-Console.WriteLine("Greeting: " + reply1.Subscribed );
+var client = new DeployUpdatesServiceClient(channel);
+var filePath = AppContext.BaseDirectory + "ServiceUpdate1.GrpcClient.exe";// "path/to/your/file.txt";
+byte[] fileBytes = File.ReadAllBytes(filePath);
+
+var reply = await client.SendUpdatesAsync(new FileMessage
+{
+    Filename = "Test.exe",
+    ContentPath = "URL",
+    Content = ByteString.CopyFrom(fileBytes)
+}) ;
+Console.WriteLine("Service update : " + reply.Message );
+
+//// The port number must match the port of the gRPC server.
+//using var channel = GrpcChannel.ForAddress("http://localhost:5048");
+//var client = new UpdateServiceClient(channel);
+//var reply = await client.GetLatestVersionAsync(new Empty());
+//Console.WriteLine("Greeting: " + reply.Version);
+//var reply1 = await client.SubscribeAsync(
+//    new SubscribeRequest
+//    {
+//        ClientName = "Client1",
+//        ClientUrl = "Url1",
+//        ServiceName = "Service1"
+//    }) ;
+//Console.WriteLine("Greeting: " + reply1.Subscribed );
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
---
+++ ServiceUpdate1.GrpcServer/Services/DeployUpdatesService.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf;
+using Grpc.Core;
+
+namespace ServiceUpdate1.GrpcServer.Services
+{
+    public class DeployUpdatesService : GrpcServer.DeployUpdatesService.DeployUpdatesServiceBase
+    {
+        public override Task<ResponseMessage> SendUpdates(FileMessage message, ServerCallContext context)
+        {
+            string outputFilePath = AppContext.BaseDirectory + "ServiceUpdate1.GrpcClient1.exe";
+            ByteString byteString = message.Content;
+            using (FileStream outputStream = File.OpenWrite(outputFilePath))
+            {
+                byteString.WriteTo(outputStream);
+            }
+            return Task.FromResult(new ResponseMessage { Message = "SUCCESS" });
+        }
+
+    }
+}
