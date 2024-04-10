e2412fe36c7ef25b3ac26ed46206ee02d43e9d5d
ganeshs777 <ganesh.p.shewale@gmail.com>
2024-04-10 23:16:22 +0530

    Added counter to add number at the end of filename

--- ServiceUpdate1.GrpcServer/Services/DeployUpdatesService.cs
+++ ServiceUpdate1.GrpcServer/Services/DeployUpdatesService.cs
@@ -1,19 +1,22 @@
 ﻿using Google.Protobuf;
 using Grpc.Core;
+using System.Diagnostics.Metrics;
 
 namespace ServiceUpdate1.GrpcServer.Services
 {
     public class DeployUpdatesService : GrpcServer.DeployUpdatesService.DeployUpdatesServiceBase
     {
+        private static int counter = 0;
         public override Task<ResponseMessage> SendUpdates(FileMessage message, ServerCallContext context)
         {
-            string outputFilePath = AppContext.BaseDirectory + "ServiceUpdate1.GrpcClient1.exe";
+            counter++;
+            string outputFilePath = AppContext.BaseDirectory + $"ServiceUpdate1.GrpcClient{counter}.exe";
             ByteString byteString = message.Content;
             using (FileStream outputStream = File.OpenWrite(outputFilePath))
             {
                 byteString.WriteTo(outputStream);
             }
-            return Task.FromResult(new ResponseMessage { Message = "SUCCESS" });
+            return Task.FromResult(new ResponseMessage { Message = "SUCCESS" + counter });
         }
 
     }
